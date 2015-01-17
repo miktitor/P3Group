0210c44fcf204be86a8cf8bce1bbcc9ad02ebec9
JohnRCC <johnrcc@JRCCNetbook.(none)>
2015-01-17 15:53:48 +0000

    updated test to check for invalid arguments

--- test.cpp
+++ test.cpp
@@ -7,79 +7,86 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	float smin=strtod(argv[1],NULL);
-	float smax=strtod(argv[2],NULL);
-	float ds=strtod(argv[3],NULL);
-	float r=strtod(argv[4],NULL);
-	int iter=strtod(argv[5],NULL);
 
-	//get matrix size
-	int matsize = (((float)smax-smin)/ds) - fmod((smax-smin)/ds,1);
-
-	float vals[matsize][matsize][iter];
-
-	int i,j,k;
-
-	//generate initial matrix
-	for(k=0;k<iter;k++)
+  if(argc<2)
+  {
+    cout<<"Usage: ./test [Min x/y val][Max x/y val][x/y divisions (ds)][Circle Radius][No. Iterations]\n";
+    return 1;
+  }
+  
+  float smin=strtod(argv[1],NULL);
+  float smax=strtod(argv[2],NULL);
+  float ds=strtod(argv[3],NULL);
+  float r=strtod(argv[4],NULL);
+  int iter=strtod(argv[5],NULL);
+  
+  //get matrix size
+  int matsize = (((float)smax-smin)/ds) - fmod((smax-smin)/ds,1);
+  
+  float vals[matsize][matsize][iter];
+  
+  int i,j,k;
+  
+  //generate initial matrix
+  for(k=0;k<iter;k++)
+    {
+      for(i=0;i<matsize;i++)
 	{
-		for(i=0;i<matsize;i++)
+	  for(j=0;j<matsize;j++)
+	    {
+	      if(i==0)
+		vals[i][j][k]=1;
+	      else if(i==matsize-1)
+		vals[i][j][k]=-1;
+	      else
 		{
-			for(j=0;j<matsize;j++)
-			{
-				if(i==0)
-					vals[i][j][k]=1;
-				else if(i==matsize-1)
-					vals[i][j][k]=-1;
-				else
-				{
-					vals[i][j][k]=0;
-				}
-			}
+		  vals[i][j][k]=0;
 		}
+	    }
 	}
-
-	//print matrix
-	/*for(i=0;i<=matsize-1;i++)
+    }
+  
+  //print matrix
+  /*for(i=0;i<=matsize-1;i++)
+    {
+    for(j=0;j<=matsize-1;j++)
+    {
+    cout<<vals[i][j][0]<<" ";
+    }
+    cout<<"\n";
+    }*/
+  //run algorithm to get average of values
+  for(k=1;k<iter;k++)
+    {
+      for(i=1;i<matsize-1;i++)
 	{
-		for(j=0;j<=matsize-1;j++)
+	  for(j=0;j<matsize;j++)
+	    {
+	      if(j==0)
 		{
-			cout<<vals[i][j][0]<<" ";
+		  vals[i][j][k] = (vals[i-1][j][k-1]+vals[i+1][j][k-1]+vals[i][j+1][k-1])/3.00;
 		}
-		cout<<"\n";
-	}*/
-	//run algorithm to get average of values
-	for(k=1;k<iter;k++)
-	{
-		for(i=1;i<matsize-1;i++)
+	      else if(j==matsize)
 		{
-			for(j=0;j<matsize;j++)
-			{
-				if(j==0)
-				{
-					vals[i][j][k] = (vals[i-1][j][k-1]+vals[i+1][j][k-1]+vals[i][j+1][k-1])/3.00;
-				}
-				else if(j==matsize)
-				{
-					vals[i][j][k] = (vals[i-1][j][k-1]+vals[i+1][j][k-1]+vals[i][j-1][k-1])/3.00;
-				}
-				else
-				{
-					vals[i][j][k] = 0.25*(vals[i+1][j][k-1]+vals[i-1][j][k-1]+vals[i][j+1][k-1]+vals[i][j-1][k-1]);
-				}
-			}
+		  vals[i][j][k] = (vals[i-1][j][k-1]+vals[i+1][j][k-1]+vals[i][j-1][k-1])/3.00;
 		}
+	      else
+		{
+		  vals[i][j][k] = 0.25*(vals[i+1][j][k-1]+vals[i-1][j][k-1]+vals[i][j+1][k-1]+vals[i][j-1][k-1]);
+		}
+	    }
 	}
-
-	for(i=0;i<matsize;i++)
+    }
+  
+  for(i=0;i<matsize;i++)
+    {
+      for(j=0;j<matsize;j++)
 	{
-		for(j=0;j<matsize;j++)
-		{
-			cout<<vals[i][j][iter-2]<<" ";
-		}
-		cout<<"\n";
+	  cout<<vals[i][j][iter-2]<<" ";
 	}
-
-
-	return 0;
+      cout<<"\n";
+    }
+  
+  
+  return 0;
 }
